412aadf13075f100ef7bd56c1a6bb1eabbfca7d4
Filonov <ABFilonov@yandex.ru>
2025-02-19 17:25:18 +0200

    Второе задание готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,52 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let avg = Number(((arr.reduce((acc, num) =>acc += num, 0)) / arr.length).toFixed(2));
+    return { min: min, max: max, avg: avg };
 }
 
-function summElementsWorker(...arr) {
 
+function summElementsWorker(...arr) {
+  return arr.length === 0 ? 0 : arr.reduce((acc, num) => acc += num, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  return arr.length === 0 ? 0 : Math.max(...arr) - Math.min(...arr);
 }
 
-function differenceEvenOddWorker(...arr) {
 
+function differenceEvenOddWorker(...arr) {
+  let sumEvenElement = arr.reduce((acc, num) => {
+    if(num % 2 === 0){
+      acc += num;
+    }
+    return acc
+  },0);
+
+  let sumOddElement = arr.reduce((acc, num) => {
+    if(num % 2 !== 0){
+      acc += num;
+    }
+    return acc
+  },0);
+  return arr.length === 0 ? 0 : sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  let sumEvenElement = arr.reduce((acc,num) => {
+    if(num % 2 === 0){
+      acc.push(num)
+    }
+    return acc;
+  },[]);
+  return arr.length === 0 ? 0 : (sumEvenElement.reduce((acc, num) => acc += num, 0) / sumEvenElement.length);
 }
 
 function makeWork (arrOfArr, func) {
-
+  let workerResult = [];
+  arrOfArr.forEach((arr) => {
+    workerResult.push(func(...arr));
+  })
+  return arrOfArr.length === 0 ? 0 : Math.max(...workerResult);
 }
+
