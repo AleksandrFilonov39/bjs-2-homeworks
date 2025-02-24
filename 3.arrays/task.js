157c63cfd27ee01bf1a339f57c416a41e1841057
Filonov <ABFilonov@yandex.ru>
2025-02-24 17:47:18 +0200

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+    if(arr1.length !== arr2.length){
+        return false;
+    }else {
+  return arr1.every((number, ind) => number === arr2[ind]);
+        }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+    return users.filter((user) => user.gender === gender).reduce((acc, age, ind, arr) => {
+        acc += arr[ind].age;
+        if(ind === arr.length - 1) {
+            return acc / arr.length
+        }
+         return acc;
+    },0)
+  }
   
-}
